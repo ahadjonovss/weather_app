2ed583d87ad7f41e696d2a63780f59ae1d3fadc5
Samandar <samandarahadjonov@gmail.com>
2022-10-02 01:40:21 +0500

    utils has been downloaded

---
+++ android/app/src/main/kotlin/com/ahadjonovss/weather_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ahadjonovss.weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ utils/fonts.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+
+class MyFonts{
+  static const Inter_Bold = TextStyle(
+    fontFamily: 'Inter_Bold',
+    fontSize: 25,
+    fontWeight: FontWeight.w400,);
+
+  static const Inter_Medium = TextStyle(
+    fontFamily: 'Inter_Medium',
+    fontSize: 25,
+    fontWeight: FontWeight.w400,);
+
+  static const Inter_Regular = TextStyle(
+    fontFamily: 'Inter_Regular',
+    fontSize: 25,
+    fontWeight: FontWeight.w400,);
+
+  static const Inter_Thin = TextStyle(
+    fontFamily: 'Inter_Thin',
+    fontSize: 25,
+    fontWeight: FontWeight.w400,);
+}
